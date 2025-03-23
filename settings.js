ff8127d444c7c4bf2561866901c7175a1e1f5e77
zмιιх тecн <zmiixtech@gmail.com>
2025-03-23 08:04:21 +0000

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'Knight',
+  packname: 'ZMIIX',
   author: 'Bot',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  botName: "ZmiixBot",
+  botOwner: 'ZMIIX', // Your name
+  ownerNumber: '212776003604', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
